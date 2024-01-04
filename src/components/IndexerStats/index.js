b7f6006f81e55950931b251c2b1ef99fbd8024d7
x <admin@emark20.com>
2024-01-04 17:08:13 +0800

    update indexer stats

--- src/components/IndexerStats/index.js
+++ src/components/IndexerStats/index.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import useApp from "../../hooks/useApp";
+import { useMemo } from "react";
 
 const IndexerStats = () => {
   const {indexer} = useApp()
 
+  const behind = useMemo(() => {
+    if (indexer) {
+      return indexer.head - indexer.confirmations - indexer.indexer
+    }
+    return null
+  }, [indexer])
+
   return (
-    <IndexerWrapper>
-      Indexer Status: {indexer ? indexer.head - indexer.confirmations - indexer.indexer : '~'} block behind
+    <IndexerWrapper behind={behind}>
+      Indexer Status: {behind} block behind
     </IndexerWrapper>
   )
 }
@@ -15,16 +23,17 @@ export default IndexerStats
 
 const IndexerWrapper = styled.div`
   width: 100%;
-  background: rgba(236, 188, 85, 0.18);
+  background: ${({behind}) => behind >= 12 ? '#F56C6C4f' : '#E6A23C4f'};
   position: fixed;
   z-index: 100;
   top: 65px;
-  display: flex;
+  display: ${({behind}) => behind >= 2 ? 'flex' : 'none'};;
   justify-content: center;
   align-items: center;
-  color: white;
+  color: rgba(255, 255, 255, 0.6);
   font-size: 10px;
   padding: 1px 0;
+  transition: all 0.2s ease;
 
   @media (max-width: 768px) {
     display: none;
